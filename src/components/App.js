1c78f5f9efd644455f451ebdd1d995176571d834
mccannk <mccannk@gmail.com>
2021-10-20 10:42:17 -0400

    Local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RecipeList from './RecipeList'
 import '../css/app.css'
 import { v4 as uuidv4 } from 'uuid';
 
 export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'cookiwWithReact.recipes'
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes)
+
+  useEffect(() => {
+      const  recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+      if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+      }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+    }, [recipes])    
 
   const RecipeContextValue ={
     handleRecipeAdd,
@@ -16,7 +26,7 @@ function App() {
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
-      name: 'New',
+      name: 'New recipe',
       servings: 1,
       cookTime: '1:00',
       instructions: 'Instr.',
