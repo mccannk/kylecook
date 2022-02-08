2e4df931de7e43519bacb75843731a0a47b94ff9
mccannk <mccannk@gmail.com>
2022-02-08 11:43:16 -0500

    Stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/components/App';
-import Scripts from './Scripts'
+// import App from '../src/components/App';
+// import Scripts from './Scripts'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Scripts />
+    {/* <Scripts /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
