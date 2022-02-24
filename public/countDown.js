c008c70c038af68199eec0828ff4d0b3751f3e46
mccannk <mccannk@gmail.com>
2022-02-24 11:43:16 -0500

    Changes

--- public/countDown.js
+++ public/countDown.js
@@ -8,16 +8,14 @@ const second = 1000;
 const minute = second * 60
 const hour = minute * 60;
 const day = hour * 24
-
-// const textDay = Math.floor(gap / day)
 const textHour = Math.floor((gap % day) / hour);
 const textMinute = Math.floor((gap % hour) / minute);
 const textSecond = Math.floor((gap % minute) / second);
 
 // document.querySelector('.day').innerText = textDay;
-document.querySelector('.hour').innerText = textHour < 10 ? '0' + textHour : textHour;
-document.querySelector('.minute').innerText = textMinute < 10 ? '0' + textMinute : textMinute;
-document.querySelector('.second').innerText = textSecond < 10 ? '0' + textSecond : textSecond;
+document.querySelector('.demo').innerHTML = 'Hours: ' + textHour + ' , Minutes: ' + textMinute + ' , Seconds: ' + textSecond;
+// document.querySelector('.minute').innerText = textMinute < 10 ? '0' + textMinute : textMinute;
+// document.querySelector('.second').innerText = textSecond < 10 ? '0' + textSecond : textSecond;
 }
 
 setInterval(countdown, 1000)
